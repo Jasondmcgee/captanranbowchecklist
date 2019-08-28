a91f34eb618696533f50c581b662098c64196616
Jasondmcgee <jasondmcgee17@gmail.com>
2019-08-28 15:52:42 -0700

    first commit

---
+++ checklist.py
@@ -0,0 +1,19 @@
+print("Hello World")
+
+
+def my_fun_function(say_this):
+    print(say_this)
+
+
+my_fun_function('Hello World')
+
+
+def create(item):
+    checklist.append(item)
+
+
+checklist = list()
+create('Blue')
+print(checklist)
+create('Orange')
+print(checklist)
