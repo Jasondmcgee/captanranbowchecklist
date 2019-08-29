d4cc58bcefaa386ae9777068a0b2d3e1410ed390
Jasondmcgee <jasondmcgee17@gmail.com>
2019-08-29 09:34:30 -0700

    revised checklist

--- checklist.py
+++ checklist.py
@@ -1,28 +1,23 @@
 checklist = list()
 
-# CREATE
+
 def create(item):
-    # Create item code here
     checklist.append(item)
 
-# READ
+
 def read(index):
-    # Read code here
     return checklist[index]
 
 
-# UPDATE
 def update(index, item):
-    # Update code here
     checklist[index] = item
 
-# DESTROY
+
 def destroy(index):
     checklist.pop(index)
 
 
 def test():
-    # Add your testing code here
     create("purple sox")
     create("red cloak")
 
@@ -33,7 +28,6 @@ def test():
     destroy(1)
 
     print(read(0))
-    print(read(1))
 
 
 test()
